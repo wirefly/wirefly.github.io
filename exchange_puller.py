295de377103485179fe0c243ef6ff78e563e2616
Connor Killion <connor.killion@here.com>
2016-11-13 02:48:18 -0800

    Add JSON Dump

--- exchange_puller.py
+++ exchange_puller.py
@@ -1,4 +1,4 @@
-import requests
+import requests, json
 
 currency_list = ['USD', 'IDR', 'BGN', 'ILS', 'GBP', 'DKK', 'CAD', 'JPY', 'HUF', 'RON', 'MYR', 'SEK', 'SGD', 'HKD', 'AUD', 'CHF', 'KRW', 'CNY', 'TRY', 'HRK', 'NZD', 'THB', 'EUR', 'NOK', 'RUB', 'INR', 'MXN', 'CZK', 'BRL', 'PLN', 'PHP', 'ZAR']
 base_url = "https://api.fixer.io/latest?base="
@@ -8,8 +8,10 @@ def exchange_rates():
     for currency in currency_list:
         r = requests.get(base_url + currency)
         rates_dict[currency] = dict(r.json())['rates']
+        print(rates_dict[currency])
+    return rates_dict
 
 if __name__ == '__main__':
-    # rates = exchange_rates()
-    r = requests.get(base_url + currency_list[0])
-    print(dict(r.json())['rates']['HKD'])
+    rates = exchange_rates()
+    with open('rates.json', 'w') as fp:
+        json.dump(rates, fp)
