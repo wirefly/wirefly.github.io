f35ac252674f5e92d6eca25675ddc08b204e12cb
Sirjan Kafle <sxk1693@berkeley.edu>
2016-11-13 02:03:34 -0800

    Fixed more algorithms stuff

--- apis/capital_one.py
+++ apis/capital_one.py
@@ -2,31 +2,35 @@ import requests
 import json
 
 apiKey = 'f1eefaa9631867b1e47580406a2dcc83'
-def createAccount(first_name, last_name, currency, isBank):
-	url = 'http://api.reimaginebanking.com/customers/?key={}'.format(apiKey)
-	#City represents the currency that the user uses
-	payload = {
-	    "first_name": first_name,
-	    "last_name": last_name,
-	    "address": {
-	    	"state": "TL",
-	    	"zip": "12345",
-	      	"street_number": "string",
-	      	"street_name": isBank,
-	      	"city": currency}
-			
-		}
-	response = requests.post( 
-		url, 
-		data=json.dumps(payload),
-		headers={'content-type':'application/json'},
-		)
 
-	if response.status_code == 201:
-		print('account created')
-	else:
-		print("failed bitch ", response.text, response.reason)
+
+def createAccount(first_name, last_name, currency, isBank):
+    url = 'http://api.reimaginebanking.com/customers/?key={}'.format(apiKey)
+    # City represents the currency that the user uses
+    payload = {
+        "first_name": first_name,
+        "last_name": last_name,
+        "address": {
+            "state": "TL",
+            "zip": "12345",
+            "street_number": "string",
+            "street_name": isBank,
+            "city": currency}
+
+    }
+    response = requests.post(
+            url,
+            data=json.dumps(payload),
+            headers={'content-type': 'application/json'},
+    )
+
+    if response.status_code == 201:
+        print('account created')
+    else:
+        print("failed bitch ", response.text, response.reason)
+
+
 def getCustomers():
-	url = 'http://api.reimaginebanking.com/customers/?key={}'.format(apiKey)
-	response = requests.get(url)
-	print(response.text)
+    url = 'http://api.reimaginebanking.com/customers/?key={}'.format(apiKey)
+    response = requests.get(url)
+    print(response.text)
