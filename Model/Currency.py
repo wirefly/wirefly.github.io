ededaee73f623fdda7538e76869959d59e1a53ea
“Tarun <khasnavistarun.comssh-keygen -t rsa -C khasnavistarun.comgit config --global color.ui truegit config --global user.name Tarun Khasnavis>
2016-11-13 03:34:05 -0800

    Simulate and Exchange Json

--- Model/Currency.py
+++ Model/Currency.py
@@ -1,8 +1,16 @@
+import json
+import sys
+
 class Currency:
+
+    with open('rates.json') as json_data:
+        d = json.load(json_data)
+        rateDict = json.JSONDecoder().decode(d)
+
 
     """Creates currency with sender receiver and amount"""
     def __init__ (self, country):
         self.country = country
 
     def getExchangeRate(self, currency):
-        return 
+        return rateDict[self]
